814b9a2b887888b666a7f03d4078640bbfddae91
u17080534 <55683117+u17080534@users.noreply.github.com>
2020-08-06 22:24:55 +0200

    Production Fix

--- stream_server/__main__.py
+++ stream_server/__main__.py
@@ -5,7 +5,7 @@ app = create_app(debug=True)
 
 
 def main():
-    eventlet.wsgi.server(eventlet.listen(('ec2-13-245-35-130.af-south-1.compute.amazonaws.com', 8008)), app)
+    eventlet.wsgi.server(eventlet.listen(('', 8008)), app)
 
 
 if __name__ == '__main__':
