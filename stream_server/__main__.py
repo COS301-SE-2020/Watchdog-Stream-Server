a17ef86a59589b1b6e1ca683d5f14f9384f69c27
u17080534 <55683117+u17080534@users.noreply.github.com>
2020-08-18 00:50:17 +0200

    Https Fix

--- stream_server/__main__.py
+++ stream_server/__main__.py
@@ -5,7 +5,7 @@ app = create_app(debug=True)
 
 
 def main():
-    eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 8440)),
+    eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 8080)),
                                            certfile='https/certificate.crt',
                                            keyfile='https/private.key',
                                            server_side=True),
