0d2062c4402132c18d6cf30df134934413a54da5
u17080534 <55683117+u17080534@users.noreply.github.com>
2020-09-08 00:11:35 +0200

    Update __main__.py

--- stream_server/__main__.py
+++ stream_server/__main__.py
@@ -1,16 +1,16 @@
 import eventlet
+import app
 from app import create_app
 
 eventlet.monkey_patch()
 
-app = create_app()
+server_app = create_app()
 
 if __name__ == '__main__':
     try:
         app_host = '127.0.0.1'
-        app_port = 8080
-
-        eventlet.wsgi.server(eventlet.listen((app_host, int(app_port))), app)
+        app_port = 8008
+        eventlet.wsgi.server(eventlet.listen((app_host, int(app_port))), server_app)
 
     except Exception as e:
         print(e)
