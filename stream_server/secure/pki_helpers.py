2c85a345774dbc253bc02385e54a3aabca4ebee7
u17080534 <55683117+u17080534@users.noreply.github.com>
2020-09-11 01:02:15 +0200

    Update pki_helpers.py

--- stream_server/secure/pki_helpers.py
+++ stream_server/secure/pki_helpers.py
@@ -6,7 +6,7 @@ from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def generate_private_key(filename: str, passphrase: str):
+def generate_private_key(filename, passphrase):
     private_key = rsa.generate_private_key(
         public_exponent=65537, key_size=2048, backend=default_backend()
     )
