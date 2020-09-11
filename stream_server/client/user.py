676799d04d07203f8e6be41aa1566b3ce8013fed
u17080534 <55683117+u17080534@users.noreply.github.com>
2020-09-11 14:16:27 +0200

    Update Config

--- stream_server/client/user.py
+++ stream_server/client/user.py
@@ -4,6 +4,7 @@ import urllib3
 
 CLIENT_KEY = 'supersecure'
 URL = 'https://ec2-13-244-153-139.af-south-1.compute.amazonaws.com:443/'
+# URL = 'http://127.0.0.1:8008'
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
