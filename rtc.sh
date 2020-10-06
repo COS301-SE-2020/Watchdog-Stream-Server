a3d5ff538471fb1a0172e23c1909b803f650f0d4
Rishi <34397126+urishiraval@users.noreply.github.com>
2020-10-06 04:28:05 +0200

    Converted rtc.bat to rtc.sh

---
+++ rtc.sh
@@ -0,0 +1,10 @@
+python ./stream_server/client producer UID_1 PRODID_1 CAMID_1 CAMID_2 CAMID_3
+python ./stream_server/client producer UID_1 PRODID_2 CAMID_4 CAMID_5 CAMID_6 CAMID_3
+python ./stream_server/client consumer UID_1 - PRODID_1 CAMID_1 CAMID_2 - PRODID_2 CAMID_5 CAMID_6 CAMID_3
+python ./stream_server/client consumer UID_2 - PRODID_X CAMID_99 CAMID_98 - PRODID_Y CAMID_E CAMID_NEVER
+python ./stream_server/client consumer UID_2 - PRODID_X CAMID_99 CAMID_98 - PRODID_Y CAMID_E CAMID_NEVER
+python ./stream_server/client consumer UID_2 - PRODID_X CAMID_99 CAMID_98 - PRODID_Y CAMID_E CAMID_NEVER
+python ./stream_server/client consumer UID_2 - PRODID_X CAMID_99 CAMID_98 - PRODID_Y CAMID_E CAMID_NEVER
+python ./stream_server/client consumer UID_2 - PRODID_X CAMID_99 CAMID_98 - PRODID_Y CAMID_E CAMID_NEVER
+python ./stream_server/client producer UID_2 PRODID_X CAMID_99 CAMID_98 CAMID_97
+python ./stream_server/client producer UID_2 PRODID_Y CAMID_E
